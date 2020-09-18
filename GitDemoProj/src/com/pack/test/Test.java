c15233213a4914fa701c192907ea26d43d3fd418
Dev <Dev@gmail.com>
2020-09-18 12:03:06 +0530

    commited

--- GitDemoProj/src/com/pack/test/Test.java
+++ GitDemoProj/src/com/pack/test/Test.java
@@ -2,6 +2,9 @@ package com.pack.test;
 
 public class Test {
 	
+	public void m2() {
+		System.out.println("m2 method");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("main method");
